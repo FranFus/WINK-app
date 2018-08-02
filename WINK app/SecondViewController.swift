f2728d01311c3d550e5c3536386e19b2a035db8d
Francesco Fuscaldo <thebigapple97@gmail.com>
2018-08-02 15:31:30 +0200

    array filled with json data

--- WINK app/SecondViewController.swift
+++ WINK app/SecondViewController.swift
@@ -14,18 +14,23 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
     var namesArray: Array<String> = []
     var label: String = ""
     var sendName: SendNameDelegate?
+    let urlString = "https://www.mocky.io/v2/5b62bfce30000049006500ea"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Retry", style: .plain, target: self, action: #selector(retry))
+        
         let nib = UINib.init(nibName: "TableViewCell", bundle: nil)
         self.namesTableView.register(nib, forCellReuseIdentifier: "tableViewCell")
         
-        namesArray = ["Francesco", "Marco", "Luca", "Daenerys Stormborn of the House Targaryen, First of Her Name, the Unburnt, Queen of the Andals and the First Men, Khaleesi of the Great Grass Sea, Breaker of Chains, and Mother of Dragons", "Marco", "Luca", "John Snow"]
+        namesArray = []
         namesTableView.delegate = self
         namesTableView.dataSource = self
         
         self.navigationItem.title = "Choose a name"
+        
+        getNames()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -44,16 +49,16 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return true
     }
     
-     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-       
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
-                namesArray.remove(at: indexPath.row)
-    
+            namesArray.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
-     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         label = self.namesArray[indexPath.row]
         sendName?.passString(string: label)
     }
@@ -66,8 +71,49 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return 150
     }
     
+    func getNames() {
+        guard let url = NSURL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: (url as URL)) { (data, response, error) in
+            if error != nil {
+                self.namesArray.append("No item found, check the connection and retry")
+                print(error!.localizedDescription)
+            }
+            
+            guard let data = data else { return }
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            
+            if let array = json as? [Any] {
+                
+                for case let string as String in array {
+
+                    if array.count > 0 {
+                        self.namesArray.append(string)
+                    } else {
+                        self.namesArray.append("No item found")
+                        
+                    }
+                }
+            }
+            
+            //Get back to the main queue
+            DispatchQueue.main.async {
+                self.namesTableView?.reloadData()
+            }
+            
+            
+            }.resume()
+        
+        
+    }
+    
+    @objc func retry() {
+        namesArray = []
+        getNames()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
 }
